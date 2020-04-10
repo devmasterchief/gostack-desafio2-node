24bc94bbc01de78c520b9533051f43e32a71d60a
islimn <pedrohenriquedelima01@gmail.com>
2020-04-10 20:28:15 -0300

    Final commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,71 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  
+  const rep = { 
+    id: uuid(), 
+    title, 
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(rep);
+  return response.json(rep);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const index = repositories.findIndex(repository => repository.id == id);
+
+  if(index === -1){
+    return response.status(400).json({ error: 'Repository not found' });
+  }
+
+  const rep = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[index].likes,
+  };
+  
+  repositories[index] = rep;
+  return response.json(rep);
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const { id } = req.params;
+
+  const index = repositories.findIndex(repository => repository.id == id);
+
+  if(index < 0){
+    return res.status(400).json({ error: 'Repository Not Found' });
+  }else{
+    repositories.splice(index, 1);
+    return res.status(204).send();
+  }
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = repositories.findIndex(repository => repository.id == id);
+
+  if(index < 0){
+    return response.status(400).json({ error: 'Repository Not Found' });
+  }
+
+  repositories[index].likes += 1;
+
+  return response.json(repositories[index]);
 });
 
 module.exports = app;
